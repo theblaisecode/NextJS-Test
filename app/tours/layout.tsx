7796e1190a1ceffdd3f0f77be89f50ba3256f357
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-06-24 05:46:28 +0100

    first commit

--- app/tours/layout.tsx
+++ app/tours/layout.tsx
@@ -2,7 +2,7 @@ function ToursLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <header className="py-2 w1/2 bg-slate-500 rounded mb-4">
-        <h1 className="text-3xl text-white text-center">Nested Layout</h1>
+        <h1 className="text-3xl text-white text-center">Nested Tours Layout</h1>
       </header>
 
       {children}
