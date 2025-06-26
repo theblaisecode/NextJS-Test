9e9e3f2ef76019d6ada5aecdf488866fe700c660
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-06-26 06:32:00 +0100

    first commit

--- components/UsersList.tsx
+++ components/UsersList.tsx
@@ -1,4 +1,27 @@
-function UsersList() {
-  return <h3 className="text-xl">UsersList</h3>;
+import { type User } from "../types/types";
+import { fetchUsers } from "../utils/actions";
+
+async function UsersList() {
+  const users = await fetchUsers();
+
+  return (
+    <div className="mt-4 ml-6">
+      {users ? (
+        <ol className="list-decimal">
+          {users.map((item) => {
+            const { id, firstName, lastName }: User = item;
+            
+            return (
+              <li key={id} className="capitalize text-lg">
+                {firstName} {lastName}
+              </li>
+            );
+          })}
+        </ol>
+      ) : (
+        <p>No users found...</p>
+      )}
+    </div>
+  );
 }
 export default UsersList;
