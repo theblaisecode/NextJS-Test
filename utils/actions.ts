ff7666fe8983be1170139779603c72abc5c8c03e
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-06-26 06:40:30 +0100

    first commit

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,8 @@
 "use server";
 import { readFile, writeFile } from "fs/promises";
 import { type User } from "../types/types";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export const createUser = async (formData: FormData) => {
   // const rawData = Object.fromEntries(formData);
@@ -14,6 +16,8 @@ export const createUser = async (formData: FormData) => {
 
   const newUser: User = { firstName, lastName, id: Date.now().toString() };
   await savesers(newUser);
+  revalidatePath("/actions");
+  // redirect("/");
 };
 
 export const fetchUsers = async (): Promise<User[]> => {
